1a74c62a6b65416a2d249cb7ca9677563e0ec306
Cireka <tsifekide@gmail.com>
2023-07-09 02:05:40 +0400

    added stairs

--- src/app/components/canvas/Stars.tsx
+++ src/app/components/canvas/Stars.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { useState, useRef, Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Points, PointMaterial, Preload, Sphere } from "@react-three/drei";
+import * as random from "maath/random/";
 
-const Stars = () => {
+const Stars = (props: any) => {
+  const ref = useRef(null);
+  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
+
   return (
-    <div>Stars</div>
-  )
-}
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+        <PointMaterial
+          transparent
+          color="#f272c8"
+          size={"0.002"}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
+};
 
-export default Stars
+const StarsCanvas = () => {
+  return (
+    <div className="w-full h-auto absolute inset-0 z-[-1]">
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+};
+
+export default StarsCanvas;
