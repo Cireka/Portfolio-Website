7a803a43be1107f723b4cb5ebedd3938da6c943d
Cireka <tsifekide@gmail.com>
2023-07-23 17:39:21 +0400

    fixed form span

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -93,7 +93,7 @@ const Contact = () => {
             />
           </label>
           <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your Name</span>
+            <span className="text-white font-medium mb-4">Your Message</span>
             <textarea
               rows={7}
               name="message"
